42814d9e39b4ed2bc94a7d636e792571f763126c
serg4250 <sergio.pereira@rackspace.com>
2018-09-20 23:46:31 -0500

    Added Class Method scan_lights

--- http_hue_api/http_hue_api.py
+++ http_hue_api/http_hue_api.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from builtins import object
 import requests
 from prettytable import PrettyTable
-import sys
 
 
 class PyHue(object):
